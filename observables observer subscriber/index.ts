f23c4cd081cd396a3d726780c6203811a18a9bae
David <davidrpy@gmail.com>
2022-07-15 20:18:20 -0500

    observables observer subscribe

---
+++ observables observer subscriber/index.ts
@@ -0,0 +1,44 @@
+import{Observable, Observer, } from "rxjs";
+
+
+
+const myObserver: Observer<any>=
+{
+    next: x => 
+    {
+        if (!isNaN(x)) {
+            console.log(x + 10);
+
+        } else {
+            console.log("`${x}`: no es un numero, ponele voluntad");
+
+        }
+    },
+       error: err => console.error("no es asi jaja", err),
+       
+       complete:() =>console.log("mi trabajo aqui termino"),
+       
+    
+    
+};
+
+const myObservable = new Observable
+    (
+        subscriber =>
+            {
+                subscriber.next("holis");
+                subscriber.next(10);
+                subscriber.next(20);
+                subscriber.error("hola soy un error");
+            }
+    )
+
+    const myObservable2 = new Observable
+            (
+                Subscriber=>
+                    {
+                        Subscriber.complete()
+                    }
+            )
+    myObservable.subscribe(myObserver);
+    myObservable2.subscribe(myObserver);
